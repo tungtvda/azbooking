72639a948b71cf38472cf4a072b3ef0d2e8f3697
tungtv <tungtv@qsoft.com.vn>
2016-12-14 17:33:21 +0700

    upcode

--- view/default/left_danhmuc.php
+++ view/default/left_danhmuc.php
@@ -3,6 +3,10 @@ require_once DIR . '/view/default/public.php';
 function view_left_danhmuc($data = array())
 {
     $asign = array();
-
+    $asign['tintuc_left'] ='';
+    if(count($data['tintuc_left'])>0)
+    {
+        $asign['tintuc_left'] = print_item('tintuc_left', $data['tintuc_left']);
+    }
     print_template($asign, 'left_danhmuc');
 }
---
+++ view/default/template/item/tintuc_left.tpl
@@ -0,0 +1,6 @@
+<li>
+    <div class="image-wrap image-cover"><a href="{link}"><img src="{img}"
+                                                                        alt="{name}" title="{name}"></a></div>
+    <div class="content"><a href="{link}">{name}</a>
+    </div>
+</li>
