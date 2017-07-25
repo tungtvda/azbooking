92f33342727b83b9a72cd01923e9d8beeca1ccd9
tungtv <tungtv.soict@gmail.com>
2017-07-25 21:16:49 +0700

    tungtv upcode

--- tiepthi/manage/donhang_chitiet.php
+++ tiepthi/manage/donhang_chitiet.php
@@ -14,12 +14,21 @@ require_once DIR.'/controller/default/public.php';
 require_once DIR . '/common/paging.php';
 require_once DIR . '/common/redict.php';
 $data['config']=config_getByTop(1,'','');
-$id=_return_mc_decrypt(_returnGetParamSecurity('id_noti'));
-if($id=''){
+$id=_return_mc_decrypt(_returnGetParamSecurity('id'));
+if($id==''){
     redict(SITE_NAME.'/tiep-thi-lien-ket/');
 }
-$id_noti=_return_mc_decrypt(_returnGetParamSecurity('id'));
+$id_noti=_return_mc_decrypt(_returnGetParamSecurity('id_noti'));
+$array_check_noti = array(
+    'id'=>_return_mc_encrypt($id),
+    'id_noti'=>_return_mc_encrypt($id_noti),
+);
 
+$list_noti= returnCURL($array_check_noti, SITE_NAME_MANAGE.'/azbooking-get-detail-booking.html');
+$data_list_hoahong=json_decode($list_noti,true);
+if(!isset($data_list_hoahong['success'])||$data_list_hoahong['success']==0){
+    redict(SITE_NAME.'/tiep-thi-lien-ket/');
+}
 $name_module='Danh sách đơn hàng';
 $data['breadcrumb']="<li href=''>Đơn hàng</li><li class='active'>Tour</li>";
 
