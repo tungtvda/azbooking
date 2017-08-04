f75b77d445029528050b80153d7b52c787fe6000
tungtv <tungtv.soict@gmail.com>
2017-08-04 20:52:58 +0700

    tungtv upcode

--- view/default/themes/assets/js/myjs.js
+++ view/default/themes/assets/js/myjs.js
@@ -258,10 +258,256 @@ $('body').on('click','.xac_minh_2_buoc', function () {
         showNotification('top','right',4,'Lỗi! bạn vui lòng F5 và thử lại');
     }
 });
-$('body').on("input",'#input_password_old', function () {
+$('body').on("blur",'#input_password_old', function () {
     checkUserPasswordOldConfirm();
 });
-$('body').on("keyup",'#input_password_old', function () {
-    checkUserPasswordOldConfirm();
+$('body').on("input",'#input_password_old', function () {
+   $('#error_password_old').hide();
+});
+function checkUserPasswordOldConfirm(){
+    var value = $("#input_password_old").val();
+    var link = $('#site_name_manage').val() + '/az-check-pass-old.html';
+    if(value!=''){
+        $.ajax({
+            method: "POST",
+            url: link,
+            data : { // Danh sách các thuộc tính sẽ gửi đi
+                value: value,
+                form_noti: $('#form_noti').serializeArray()
+            },
+            success: function (response) {
+                response = $.parseJSON(response);
+                if (response.success == 1) {
+                    var mess="";
+                    showHiddenPasswordOldUser(1,mess)
+                }
+                else{
+                    var mess="Mật khẩu cũ không chính xác";
+                    showHiddenPasswordOldUser(0,mess)
+                }
+            }
+        });
+    }
+    else{
+        var mess="Bạn vui lòng nhập mật khẩu cũ";
+        showHiddenPasswordOldUser(0,mess)
+    }
+
+}
+function showHiddenPasswordOldUser(res,mess){
+    var error_password_old=$("#error_password_old" );
+    if (res == 1) {
+        error_password_old.hide();
+        $('#error_icon_user_password_old').hide();
+        $('#user_password_old_success_icon').show();
+        $('#input_password_old').removeClass("input-error").addClass("valid");
+    }
+    else {
+        if(res!=0)
+        {
+            mess=res;
+        }
+        $('#error_icon_user_password_old').show();
+        $('#user_password_old_success_icon').hide();
+        $('#input_password_old').addClass("input-error").removeClass("valid");
+        error_password_old.removeClass("success-color");
+        error_password_old.addClass("error-color");
+        error_password_old.html(mess);
+        error_password_old.show();
+    }
+}
+// check pass
+$('body').on("input",'#input_password', function () {
+    checkUserPassword();
+});
+$('body').on("keyup",'#input_password', function () {
+    checkUserPassword();
+});
+// check pass confirm
+$('body').on("input",'#input_password_confirm', function () {
+    checkUserPasswordConfirm();
+});
+$('body').on("keyup",'#input_password_confirm', function () {
+    checkUserPasswordConfirm();
+});
+
+// check password
+function checkUserPassword(){
+    var error_password_confirm=$("#error_password_confirm" );
+    var value = $("#input_password").val();
+    var confirm_password_dangky = $('#input_password_confirm').val();
+    if(value==''){
+        var mess='Bạn vui lòng nhập mật khẩu';
+        showHiddenPasswordUser(0,mess);
+    }else{
+        if(confirm_password_dangky!=""){
+            if(value==confirm_password_dangky)
+            {
+                error_password_confirm.hide();
+                error_password_confirm.html('');
+                $('#input_password_confirm').removeClass("input-error").addClass("valid");
+            }else{
+                error_password_confirm.show();
+                error_password_confirm.html('Hai mật khẩu không khớp');
+                $('#input_password_confirm').addClass("input-error").removeClass("valid");
+            }
+        }
+        // Must have capital letter, numbers and lowercase letters
+        var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+
+        // Must have either capitals and lowercase letters or lowercase and numbers
+        var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+
+        // Must be at least 6 characters long
+        var okRegex = new RegExp("(?=.{6,}).*", "g");
+        if (okRegex.test(value) === false) {
+            // If ok regex doesn't match the password
+            $('#error_password').removeClass().addClass('error-color').show().html('Mật khẩu tối thiểu 6 ký tự.');
+            $('#error_password').addClass('error-color-size');
+            $('#input_password').addClass("input-error").removeClass("valid");
+
+
+        } else if (strongRegex.test(value)) {
+            // If reg ex matches strong password
+            showHiddenPasswordValidUser('success_pass','Mật khẩu mạnh!');
+            //$('#power_pass').removeClass().addClass('success_pass').html('Mật khẩu mạnh!');
+        } else if (mediumRegex.test(value)) {
+            // If medium password matches the reg ex
+            showHiddenPasswordValidUser('medium_pass','Hãy khiến mật khẩu mạnh hơn với chữ in hoa, số, ký tự đặc biệt!');
+            //$('#power_pass').removeClass().addClass('medium_pass').html('Hãy khiến mật khẩu mạnh hơn với chữ in hoa, số, ký tự đặc biệt!');
+        } else {
+            // If password is ok
+            showHiddenPasswordValidUser('weak_pass','Mật khẩu yếu, hãy sử dụng số và chữ hoa!');
+            //$('#power_pass').removeClass().addClass('weak_pass').html('Mật khẩu yếu, hãy sử dụng số và chữ hoa.');
+        }
+
+        //var mess='';
+        //showHiddenPasswordUser(1,mess);
+    }
+}
+function  showHiddenPasswordValidUser(res,mess){
+    $('#error_password').removeClass().addClass(res).html(mess);
+    $('#input_password').removeClass("input-error").addClass("valid");
+    $('#error_password').addClass('error-color-size');
+    $('#error_icon_user_pass').hide();
+}
+function showHiddenPasswordUser(res,mess){
+    var error_password=$("#error_password" );
+    if (res == 1) {
+        error_password.hide();
+        $('#error_icon_user_pass').hide();
+        $('#input_password').removeClass("input-error").addClass("valid");
+    }
+    else {
+        if(res!=0)
+        {
+            mess=res;
+        }
+        $('#error_icon_user_pass').show();
+        $('#input_password').addClass("input-error").removeClass("valid");
+        error_password.removeClass("success-color");
+        error_password.addClass("error-color");
+        error_password.html(mess);
+        error_password.show();
+    }
+}
+
+// check pass confirm
+function checkUserPasswordConfirm(){
+    var error_password_confirm=$("#error_password_confirm" );
+    var value = $("#input_password_confirm").val();
+    if(value==''){
+        var mess='Bạn vui lòng xác nhận mật khẩu';
+        showHiddenPasswordConfirmUser(0,mess);
+    }else{
+        var password_dangky=$('#input_password').val();
+        if(value==password_dangky){
+            error_password_confirm.hide();
+            error_password_confirm.html('');
+            $('#error_icon_user_pass_con').hide();
+            $('#input_password_confirm').removeClass("input-error").addClass("valid");
+        }else{
+            error_password_confirm.show();
+            error_password_confirm.html('Hai mật khẩu không khớp');
+            $('#error_icon_user_pass_con').show();
+            $('#input_password_confirm').addClass("input-error").removeClass("valid");
+        }
+    }
+}
+
+function showHiddenPasswordConfirmUser(res,mess){
+    var error_password_confirm=$("#error_password_confirm" );
+    if (res == 1) {
+        error_password_confirm.hide();
+        $('#error_icon_user_pass_con').hide();
+        $('#input_password_confirm').removeClass("input-error").addClass("valid");
+    }
+    else {
+        if(res!=0)
+        {
+            mess=res;
+        }
+        $('#error_icon_user_pass_con').show();
+        $('#input_password_confirm').addClass("input-error").removeClass("valid");
+        error_password_confirm.removeClass("success-color");
+        error_password_confirm.addClass("error-color");
+        error_password_confirm.html(mess);
+        error_password_confirm.show();
+    }
+}
+
+$('body').on("click",'#submit_form_password', function () {
+    var form_data=$("#submit_chang_pass").serializeArray();
+    var error_free=true;
+    for (var input in form_data){
+        var element=$("#input_"+form_data[input]['name']);
+        var error=$("#error_"+form_data[input]['name']);
+        var valid=element.hasClass("valid");
+        if (valid==false){
+            element.addClass("input-error").removeClass("valid");
+            error.show();
+            error_free=false
+        }
+    }
+    if (error_free!=false){
+        var pass_old=$("#input_password_old").val();
+        var pass=$("#input_password").val();
+        var pass_confirm=$("#input_password_confirm").val();
+        if(pass_old!=''&&pass!=""&&pass_confirm!=""){
+            var link = $('#site_name_manage').val() + '/az-update-pass.html';
+            $.ajax({
+                method: "POST",
+                url: link,
+                data : { // Danh sách các thuộc tính sẽ gửi đi
+                    pass_old : pass_old,
+                    pass: pass,
+                    pass_confirm: pass_confirm,
+                    form_noti: $('#form_noti').serializeArray()
+                },
+                success: function (response) {
+                    response = $.parseJSON(response);
+                    if(response.success==1){
+                        showNotification('top','right',2,'Cập nhật mật khẩu thành công');
+                        $('#input_password_old').val('');
+                        $('#input_password').val('');
+                        $('#input_password_confirm').val('');
+                    }else{
+                        showNotification('top','right',4,response.mess);
+                    }
+                }
+            });
+        }else{
+            if(pass_old==''){
+                showNotification('top','right',4,'Bạn vui lòng nhập mật khẩu cũ');
+            }
+            if(pass==''){
+                showNotification('top','right',4,'Bạn vui lòng nhập mật khẩu mới');
+            }
+            if(pass_confirm==''){
+                showNotification('top','right',4,'Bạn vui lòng xác nhận mật khẩu');
+            }
+        }
+    }
+
 });
 
