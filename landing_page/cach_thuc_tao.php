bc7bee7781dff31a97508a4fc75ba9a0deedd045
tungtv <tungtv.soict@gmail.com>
2017-08-13 15:59:27 +0700

    tungtv upcode

---
+++ landing_page/cach_thuc_tao.php
@@ -0,0 +1,103 @@
+<div style="padding-top: 50px" id="layers-widget-apptech-service-id-6"
+     class="widget layers-content-widget at-service-area content-vertical-massive  block-lg block-md block-sm block-xs  ">
+    <div class="container list-grid">
+        <div style="margin-bottom: 20px" class="section-title clearfix medium text-center ">
+            <h1 class="heading">
+                CÁCH THỨC TẠO THU NHẬP VỚI <span style="color: #f70000;">AZBOOKING.VN</span></h1>
+            <div class="excerpt"><p>Cho dù chỉ là Bloger - Facebooker vẫn có thể tạo nguồn thu nhập không giới hạn nhờ online với Azbooking.vn qua 3 bước đơn giản.</p></div>
+        </div>
+        <div class="grid">
+
+            <div id="layers-widget-apptech-service-id-6-270"
+                 class="layers-masonry-column layers-widget-apptech-service-id-270 span-4  first  column ">
+
+                <div class="media image-top medium">
+
+                    <div class="single-item res-sm media-body text-center">
+
+                        <div class="single-item-icon">
+                            <i class="zmdi zmdi-sign-in"></i>
+                        </div>
+
+                        <h2 class="heading ht-pt">
+                            ĐĂNG KÝ THÀNH VIÊN </h2>
+
+                        <div class="excerpt"><p>Lorem ipsum dolor sit amet, consectetur adipising elit.
+                                Pellentesque vel volutpat felis, eu condimtum ma Pellentesque mollis eros vel. </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="layers-widget-apptech-service-id-6-45"
+                 class="layers-masonry-column layers-widget-apptech-service-id-45 span-4    column ">
+
+                <div class="media image-top medium">
+
+                    <div  class="single-item res-sm media-body text-center">
+
+                        <div class="single-item-icon">
+                            <i class="zmdi zmdi-share"></i>
+                        </div>
+
+                        <h2 class="heading ht-pt">
+                            LẤY LINK TIẾP THỊ </h2>
+
+                        <div class="excerpt"><p>Lorem ipsum dolor sit amet, consectetur adipising elit.
+                                Pellentesque vel volutpat felis, eu condimtum ma Pellentesque mollis eros vel.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="layers-widget-apptech-service-id-6-921"
+                 class="layers-masonry-column layers-widget-apptech-service-id-921 span-4  last  column ">
+
+                <div class="media image-top medium">
+
+                    <div class="single-item res-sm media-body text-center">
+
+                        <div class="single-item-icon">
+                            <i class="zmdi zmdi-money"></i>
+                        </div>
+
+                        <h2 class="heading ht-pt">
+                            NHẬN HOA HỒNG </h2>
+
+                        <div class="excerpt"><p>Lorem ipsum dolor sit amet, consectetur adipising elit.
+                                Pellentesque vel volutpat felis, eu condimtum ma Pellentesque mollis eros vel.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div><!-- /row -->
+    </div>
+    <style type="text/css"> /* INLINE WIDGET CSS */
+        #layers-widget-apptech-service-id-6 {
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        #layers-widget-apptech-service-id-6-270 {
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        #layers-widget-apptech-service-id-6-45 {
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        #layers-widget-apptech-service-id-6-921 {
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+    </style>
+</div>
+<a name="about"></a>
---
+++ landing_page/tai_sao.php
@@ -0,0 +1,67 @@
+<div class="choose-area">
+    <div id="layers-widget-apptech_choosing_reason_id-10"
+         class="widget layers-post-widget apptech-feature-widget apptech-whychoos-widget clearfix  block-lg block-md block-sm block-xs  ">
+
+
+        <div class="container clearfix">
+            <div class="section-title clearfix medium text-center ">
+                <h1 class="heading">
+                    tại sao lựa chọn <span style="color: #f70000;">AZBOOKING.VN</span></h1>
+                <div class="excerpt"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                        vel volutpat felis, eu condimentum massa. Pellentesqfue mollis eros vel mattis tempor.
+                        Aliquam</p></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="grid">
+                <div class="span-7 column">
+                    <div class="choose-img">
+                        <img src="themes/wp-content/uploads/2016/11/3.png" alt="img"/>
+                    </div>
+                </div>
+                <div class="span-5 column">
+                    <div class="feature-list">
+                        <div class="single-features-list2 mb-52">
+                            <div class="feature-list-icon2">
+                                <i class="zmdi zmdi-badge-check"></i>
+                            </div>
+                            <div class="feature-list-text2">
+                                <h3>Miễn phí toàn bộ dịch vụ hỗ trợ</h3>
+                            </div>
+                        </div>
+                        <div class="single-features-list2 mb-52">
+                            <div class="feature-list-icon2">
+                                <i class="zmdi zmdi-badge-check"></i>
+                            </div>
+                            <div class="feature-list-text2">
+                                <h3>Không áp đặt doanh số, thanh toán đảm bảo</h3>
+                            </div>
+                        </div>
+                        <div class="single-features-list2 mb-52">
+                            <div class="feature-list-icon2">
+                                <i class="zmdi zmdi-badge-check"></i>
+                            </div>
+                            <div class="feature-list-text2">
+                                <h3>Tự do lựa chọn chiến dịch quảng cáo cho riêng mình</h3>
+                            </div>
+                        </div>
+                        <div class="single-features-list2 mb-52">
+                            <div class="feature-list-icon2">
+                                <i class="zmdi zmdi-badge-check"></i>
+                            </div>
+                            <div class="feature-list-text2">
+                                <h3>Hoa hồng cao nhất thị trường</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <style type="text/css"> /* INLINE WIDGET CSS */
+            #layers-widget-apptech_choosing_reason_id-10 {
+                background-repeat: no-repeat;
+                background-position: center;
+            }
+        </style>
+    </div>
+</div>
