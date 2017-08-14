2265289aa49919452179627eedd08783fff6a61d
tungtv <tungtv@qsoft.com.vn>
2017-08-14 08:22:41 +0700

    tungtv upcode

---
+++ landing_page/cau_hoi.php
@@ -0,0 +1,108 @@
+<div id="layers-widget-column-11" class="widget layers-content-widget content-vertical-massive   ">
+
+
+    <div class="container clearfix">
+        <div class="section-title clearfix medium text-center ">
+            <h3 class="heading">
+                Câu hỏi <span style="color: #f70000;">AZBOOKING.VN</span> </h3>
+            <div class="excerpt"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel
+                    volutpat felis, eu condimentum massa. Pellentesque mollis eros vel mattis tempor. Aliquam</p>
+            </div>
+        </div>
+    </div>
+    <div class="container list-grid">
+        <div class="grid">
+
+            <div id="layers-widget-column-11-576"
+                 class="layers-masonry-column layers-widget-column-576 span-6  first  column ">
+
+                <div class="media image-top medium">
+
+                    <div class="media-body text-left">
+                        <div class="excerpt">
+                            <div id="accordion">
+                                <h3 id="291" class="panel-title">Tiếp thị liên kết của AZBOOKING.VN là gì?</h3>
+                                <div>
+                                    <p id="291">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                                        vel volutpat felis, eu condimentum massa. Pellentesque mollis eros vel
+                                        mattis tempor. Aliquam
+                                        ... <a target="_blank" style="color: #1879fd" href="http://azbooking.vn/tiep-thi-lien-ket-info/gioi-thieu.html">Chi tiết <i class="fa fa-angle-double-right "></i></a>
+                                    </p>
+                                </div>
+                                <h3 id="292" class="panel-title">Làm thế nào để tham gia vào AZBOOKING.VN?</h3>
+                                <div>
+                                    <p id="292">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                                        vel volutpat felis, eu condimentum massa. Pellentesque mollis eros vel
+                                        mattis tempor. Aliquam.
+                                        ... <a target="_blank" style="color: #1879fd" href="http://azbooking.vn/tiep-thi-lien-ket-info/cac-buoc-tham-gia.html">Chi tiết <i class="fa fa-angle-double-right "></i></a>
+                                    </p>
+                                </div>
+                                <h3 id="293" class="panel-title">AZBOOKING.VN cam kết những gì?</h3>
+                                <div>
+                                    <p id="293">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                                        vel volutpat felis, eu condimentum massa. Pellentesque mollis eros vel
+                                        mattis tempor. Aliquam
+                                        ... <a target="_blank" style="color: #1879fd" href="http://azbooking.vn/tiep-thi-lien-ket-info/cam-ket.html">Chi tiết <i class="fa fa-angle-double-right "></i></a>
+                                    </p>
+                                </div>
+                                <h3 id="294" class="panel-title">Điều khoản & chính sách của AZBOOKING.VN là gì?</h3>
+                                <div>
+                                    <p id="294">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                                        vel volutpat felis, eu condimentum massa. Pellentesque mollis eros vel
+                                        mattis tempor. Aliquam
+                                        ... <a target="_blank" style="color: #1879fd" href="http://azbooking.vn/tiep-thi-lien-ket-info/cam-ket.html">Chi tiết <i class="fa fa-angle-double-right "></i></a>
+                                    </p>
+                                </div>
+                                <h3 id="294" class="panel-title">AZBOOKING.VN có hợp đồng không?</h3>
+                                <div>
+                                    <p id="295">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                                        vel volutpat felis, eu condimentum massa. Pellentesque mollis eros vel
+                                        mattis tempor. Aliquam
+                                        ... <a target="_blank" style="color: #1879fd" href="http://azbooking.vn/tiep-thi-lien-ket-info/hop-dong-cong-tac-vien.html">Chi tiết <i class="fa fa-angle-double-right "></i></a>
+                                    </p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="layers-widget-column-11-475"
+                 class="layers-masonry-column layers-widget-column-475 span-6  last  column  has-image">
+
+                <div class="media image-top medium">
+                    <div class="media-image ">
+                        <img width="450" height="372" src="themes/wp-content/uploads/2016/11/app4.png"
+                             class="attachment-full size-full" alt=""
+                             srcset="http://bestcareerbd.com/themes/apptech/wp-content/uploads/2016/11/app4.png 450w, http://bestcareerbd.com/themes/apptech/wp-content/uploads/2016/11/app4-300x248.png 300w"
+                             sizes="(max-width: 450px) 100vw, 450px"/></div>
+
+                </div>
+            </div>
+        </div><!-- /row -->
+    </div>
+    <style type="text/css"> /* INLINE WIDGET CSS */
+        #layers-widget-column-11 {
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        #layers-widget-column-11-576 {
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        #layers-widget-column-11-475 {
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+    </style>
+</div>
+<a name="screenshots"></a>
