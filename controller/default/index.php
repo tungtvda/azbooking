fe2937ecf076edc339e5c2ac7262588cc9804ed3
tungtv <tungtv@qsoft.com.vn>
2017-09-06 08:21:23 +0700

    tungtv upcode

--- controller/default/index.php
+++ controller/default/index.php
@@ -14,18 +14,9 @@ require_once DIR.'/controller/default/public.php';
 $data['menu']=menu_getByTop('','','');
 $data['config']=config_getByTop(1,'','');
 ////
-$data['tour_PROMOTIONS']=tour_getByTop(5,'promotion=1 ','id desc');
-$data['tour_sales']=tour_getByTop(5,'price_sales!="" ','id desc');
-$data['tintuc_index']=news_getByTop(5,'','id desc');
-$date_now=_returnGetDateTime();
-$data['tour_count_down']=tour_getByTop(3,'count_down!="" and count_down>"'.$date_now.'"','id desc');
 
-$data['khachsan_index']=khachsan_getByTop(9,'highlights=1 ','id desc');
 $data['video_index']=video_getByTop(1,'','id desc');
-
-$data['tieuchi']=tieuchi_getByTop('','','position asc');
-
-
+$data['slide']=slide_getByTop('','','Id desc');
 $title=$data['menu'][0]->title;
 $description=$data['menu'][0]->description;
 $keyword=$data['menu'][0]->keyword;
---
+++ view/default/template/item/slide_index.tpl
@@ -0,0 +1,12 @@
+<div class="swiper-slide dark">
+    <div class="swiper-background" style="background-image:url({img});">
+        <div class="vertical-align-middle container">
+            <h2 class="swiper-title">{name}</h2>
+            <!--<div class="swiper-desc">
+                Perfect choice for everyone. Lot`s of great features &<br/>
+                many fresh and well done pages layouts.<br/>See what you can do with Falco.
+            </div>-->
+            <a class="button button_large swiper-button" href="{link}">Chi tiết</a>
+        </div>
+    </div>
+</div>
