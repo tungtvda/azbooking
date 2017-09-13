09f1ca7dd4745902c6cd2644eb087fd702554386
tungtv <tungtv.soict@gmail.com>
2017-09-13 20:19:47 +0700

    tungtv upcode

--- landing_page/video.php
+++ landing_page/video.php
@@ -1,11 +1,11 @@
-<div hidden id="layers-widget-apptechvideo-28"
+<div  id="layers-widget-apptechvideo-28"
      class="widget apptech-video-widget content-vertical-massive  block-lg block-md block-sm block-xs   no-inset-top no-inset-bottom "
      data-black-overlay="5">
 
 
     <div class="video-area row container ">
         <div class="span-12 column text-center">
-            <a href="https://youtu.be/ZdJ7zga2yGg" data-type="youtube" class="video-popup"><i
+            <a href="https://youtu.be/E6d6K9c7t-Q" data-type="youtube" class="video-popup"><i
                     class="zmdi zmdi-play"></i></a>
         </div>
     </div>
