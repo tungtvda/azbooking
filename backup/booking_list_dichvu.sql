650f8db8362f6a803a745f2781dbecbfc13f6fa5
tungtv <tungtv@qsoft.com.vn>
2018-01-23 09:31:08 +0700

    tungtv upcode

--- backup/booking_list_dichvu.sql
+++ backup/booking_list_dichvu.sql
@@ -14,3 +14,4 @@ delete from tour_list_dichvu where tour_id=262;
 delete from tour_list_dichvu where tour_id=263;
 delete from tour_list_dichvu where tour_id=77;
 delete from tour_list_dichvu where tour_id=77;
+delete from tour_list_dichvu where tour_id=77;
