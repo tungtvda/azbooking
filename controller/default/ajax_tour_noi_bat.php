913d89afa11b8eabdea97ad8e1ccf44f0eaf0b3e
tungtv <tungtv@qsoft.com.vn>
2018-01-26 15:41:45 +0700

    Finish tungtv

--- controller/default/ajax_tour_noi_bat.php
+++ controller/default/ajax_tour_noi_bat.php
@@ -1,5 +1,7 @@
 <?php
 if(!defined('DIR')) require_once '../../config.php';
+require_once DIR . '/controller/default/public.php';
+require_once DIR . '/model/tourService.php';
 require_once DIR . '/model/tourService.php';
 $data_tour_sales = tour_getByTop(6, 'price_sales!="" and status=1', 'id desc');
 //if(count($data_tour_sales)<=0){
@@ -19,6 +21,17 @@ foreach ($data_tour_sales as $row){
     {
         $string_re.=' <div class="row" style="width: 100%; float: left; ">';
     }
+    $data_danhmuc_1 = danhmuc_1_getById($row->DanhMuc1Id);
+    $data_danhmuc_2 = danhmuc_2_getById($row->DanhMuc2Id);
+    $name_url_dm1 = '';
+    if (count($data_danhmuc_1) > 0) {
+        $name_url_dm1 = $data_danhmuc_1[0]->name_url;
+    }
+    $name_url_dm2 = '';
+    if (count($data_danhmuc_2) > 0) {
+        $name_url_dm2 = $data_danhmuc_2[0]->name_url;
+    }
+    $link_tour_list = link_tourdetail($row, $name_url_dm1, $name_url_dm2);
 
     $string_re.=' <div class="col-lg-4 col-md-6"
                                                  style="margin-bottom: 30px;padding-right: 15px; padding-left: 15px;width: 29.7%;float: left">
@@ -29,28 +42,28 @@ foreach ($data_tour_sales as $row){
 
                                                         <div class="blog-image"
                                                              style="max-height: 200px;    overflow: hidden;">
-                                                            <img style="width: 100%;"
+                                                           <a href="'.$link_tour_list.'"> <img style="width: 100%;"
                                                                  src="'._returnCheckLinkImg($row->img).'"
-                                                                 alt="'.$row->name.'" title="'.$row->name.'"></div>
+                                                                 alt="'.$row->name.'" title="'.$row->name.'"></a></div>
                                                         <div class="blog-info">
 
                                                             <h4 class="'.$row->name.'"
                                                                 style="padding: 10px; font-size: 14px;white-space: nowrap;text-overflow: ellipsis;    overflow: hidden;margin-bottom: 5px;margin-top: 5px;">
                                                                 <a style="text-decoration: none;color:#0275d8;"
-                                                                   href="#">'.$row->name.'</a></h4>
+                                                                   href="'.$link_tour_list.'">'.$row->name.'</a></h4>
                                                             <p style="    font-weight: normal;margin-top: 0px; font-size: 14px">
                                                                 Thời gian: '. $row->durations.'</p>
                                                             <ul class="post-footer"
                                                                 style="bottom: 0;left: 0;right: 0;    text-align: center;    padding: 0px;margin: 0;">
                                                                 <li style="width: 49.6%;margin: 0px; display: inline-block;border-right: 1px solid #fff;background: #EDF3F3;float: left;">
                                                                     <a style="line-height: 45px;width: 100%;    display: inline-block;text-decoration: none;    color: #a50505;font-size: 14px;"
-                                                                       href="#">
+                                                                       href="'.$link_tour_list.'">
                                                                         '.$price.'
                                                                     </a>
                                                                 </li>
                                                                 <li style="width: 50%;margin: 0px; display: inline-block;background: #EDF3F3;float: left;">
                                                                     <a style="line-height: 45px;width: 100%;    display: inline-block;text-decoration: none;    font-size: 12px;    color: #7e7c7c;"
-                                                                       href="#">
+                                                                       href="'.$link_tour_list.'">
                                                                         <del>'.$price_list_sales.'</del>
                                                                     </a>
                                                                 </li>
