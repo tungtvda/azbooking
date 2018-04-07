2f539c9d138e55142e57205c71701600a1d6602a
nuongphui <nuongphui.nhungchuyendi@gmail.com>
2018-04-07 21:19:25 +0700

    tungtv upcode

--- view/default/themes/js/review.js
+++ view/default/themes/js/review.js
@@ -1,20 +1,37 @@
 
 // requestAnimationFrame polyfill
 (function() {
-    sliderPoint('program_slide','program_point');
-    sliderPoint('tour_guide_full_slide','tour_guide_full_point');
-    sliderPoint('tour_guide_local_slide','tour_guide_local_point');
-    sliderPoint('hotel_slide','hotel_point');
-    sliderPoint('restaurant_slide','restaurant_point');
-    sliderPoint('transportation_slide','transportation_point');
+    if($('#program_slide').length){
+        sliderPoint('program_slide','program_point');
+        sliderPoint('tour_guide_full_slide','tour_guide_full_point');
+        sliderPoint('tour_guide_local_slide','tour_guide_local_point');
+        sliderPoint('hotel_slide','hotel_point');
+        sliderPoint('restaurant_slide','restaurant_point');
+        sliderPoint('transportation_slide','transportation_point');
 
-    function sliderPoint(id_range, id_point){
-        var slider = document.getElementById(id_range);
-        var output = document.getElementById(id_point);
-        output.innerHTML = slider.value; // Display the default slider value
-        slider.oninput = function() {
-            output.innerHTML = this.value;
+        function sliderPoint(id_range, id_point){
+            var slider = document.getElementById(id_range);
+            var output = document.getElementById(id_point);
+            output.innerHTML = slider.value; // Display the default slider value
+            slider.oninput = function() {
+                output.innerHTML = this.value;
+            }
         }
     }
+
+    $('body').on("click", '#show_hide_statis', function () {
+       $('#tooltip_score_distribution').slideToggle();
+    });
+    $('body').on("click", '.icon_show_hide_form_review', function () {
+        $('#form_review_show_hide').slideToggle();
+    });
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        minDate: '0',
+        startDate: '-3d',
+    });
+    $('body').on("input",'.valid_input',function(){
+
+    });
 }());
 
